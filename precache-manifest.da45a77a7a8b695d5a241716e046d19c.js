5428386b097a3656f86fbe043d50cf6daf20f531
Yana Kh <yanakarlamv@gmail.com>
2022-05-18 02:20:10 +0300

    Updates

---
+++ precache-manifest.da45a77a7a8b695d5a241716e046d19c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b72985ddc8615aca23470740df52088",
+    "url": "/react-todo-list-app/index.html"
+  },
+  {
+    "revision": "e206d74ca11d28e89bba",
+    "url": "/react-todo-list-app/static/css/main.cf504cac.chunk.css"
+  },
+  {
+    "revision": "bb6dc03909c08dc7e1c8",
+    "url": "/react-todo-list-app/static/js/2.deff7d6a.chunk.js"
+  },
+  {
+    "revision": "e206d74ca11d28e89bba",
+    "url": "/react-todo-list-app/static/js/main.1e0dc512.chunk.js"
+  },
+  {
+    "revision": "a41bdece590ff976f0b6",
+    "url": "/react-todo-list-app/static/js/runtime~main.f562617e.js"
+  }
+]);
