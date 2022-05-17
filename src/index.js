a5f2127344afc7d2f33d6a9234c06b97d779967c
Yana Kh <yanakarlamv@gmail.com>
2022-05-18 01:59:19 +0300

    2nd comm

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-// import "./index.scss";
-// import "react-todo-list-app/blob/master/src/index.scss";
-import "http://YanaKharlamova.github.io/react-todo-list-app/blob/master/src/index.scss";
-
+import "./index.scss";
 import App from "./App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
